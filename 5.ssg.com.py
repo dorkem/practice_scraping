9e0f3138887be160705eeb826b2ee48e8914ac08
dorkem <chlwogur1000@gmail.com>
2024-04-01 00:05:35 +0900

    ssg끝

--- 5.ssg.com.py
+++ 5.ssg.com.py
@@ -8,18 +8,26 @@ headers={
 }
 
 req = requests.get(url, headers=headers)
-
 html = req.text
-
 soup = BeautifulSoup(html, "html.parser")
+
+
 
 evt_osmu_lst = soup.select_one(".evt_osmu_lst")
 
 eo_link = evt_osmu_lst.select(".eo_link")
-
+ 
 for unit in eo_link:
     link = unit["href"]
     if link.startswith("https"):
         print(link)
     else:
         print(f"https://www.ssg.com{link}")
+        
+    eo_in = unit.select_one(".eo_in")
+    
+    text_list = eo_in.find_all(string=True)
+    for text in text_list:
+        if text != "\n":
+            print(text)
+    print()
